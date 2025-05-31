c6b1b0167351811c30f015eeda5b75b165fd2045
Khushi-Allawadi <khushiallawadi251@gmail.com>
2025-05-31 14:04:02 +0530

    ✅ Day 1 - Uploaded basic Python code

---
+++ Day01/main.py
@@ -0,0 +1,7 @@
+print('Mark')
+print("September 20th 2029")
+print("I am signing up for Replit's 100 days of Python challenge!")
+print("I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.")
+print("I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!")
+print("I am felling 🥹")
+print("You can follow my progress at replit.com/@Mark")
