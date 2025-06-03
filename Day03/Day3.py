e157a06a07ec41df5447cb1fe4ed4229ca5d90eb
Khushi-Allawadi <khushiallawadi251@gmail.com>
2025-06-03 14:22:13 +0530

    ✅ Day 3 - Completed

---
+++ Day03/Day3.py
@@ -0,0 +1,8 @@
+food = input("Enter a food: ")
+plant = input("Enter a plant: ")
+cook = input("Enter a cooking method: ")
+describe = input("What word descirbe burned food: ")
+diy = input("Enter a DIY item: ")
+
+print("MENU")
+print(cook, food, "with", describe, plant, "on a bed of", diy)
