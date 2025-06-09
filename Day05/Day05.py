55839b8b4708c1fcd6f649e1184dd65ca9031021
Khushi-Allawadi <khushiallawadi251@gmail.com>
2025-06-09 19:21:29 +0530

    ✅ Day 5 - Done and dusted

---
+++ Day05/Day05.py
@@ -0,0 +1,17 @@
+print("Let's find which character are you? Let's Start!!")
+
+spiderman = input("Do you like 'hanging out'?")
+if spiderman == "yes":
+  print("Then you are Spiderman!")
+else:
+  print("Then you are not Spiderman!")
+Korg = input("Do you have a 'gravelly' voice?")
+if Korg == "yes":
+  print("Aha! You're Korg!")
+else:
+  print("You are not Korg!")
+Marvel = input("Do you often feel 'Marvelous'?")
+if Marvel == "yes":
+  print("Aha! You're Marvel!Hey!")
+else:
+  print("You are not Marvel!")
