2c68a99da8c86bcb769df3a034727a459a46cc0c
Khushi-Allawadi <khushiallawadi251@gmail.com>
2025-06-10 18:48:26 +0530

    ✅ Day 6 - Completed

---
+++ Day06/Day06.py
@@ -0,0 +1,18 @@
+print("MY LOGIN SYSTEM")
+print("=====================")
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+if username == "Khushi" and password == "Khushi123":
+  print("Welcome to my system!")
+  print("You have successfully logged in.")
+  print("Here is a special message for you! : You are strongest person I ever now, and you deserve all the good things, good food and good life. Love you to the moon and back.")
+  print("Goodbye!")
+elif username == "Mom" and password == "besthuman":
+  print("Welcome to my system!")
+  print("You have successfully logged in.")
+  print("Here is a special message for you! : You are the best mother in the world, love you always.")
+  print("Goodbye!")
+else:
+  print("Invalid username or password. Please try again.")
+  print("Don't forget you are super cool and amazing person. Goodbye!")
+  
