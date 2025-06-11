fc04d29dc3ef0bf83d817e271bef951c60a9ffad
Khushi-Allawadi <khushiallawadi251@gmail.com>
2025-06-11 10:17:28 +0530

    ✅ Day 7 - completed

---
+++ Day07/Day07.py
@@ -0,0 +1,12 @@
+favseries = input("What is your favorite series? ")
+if favseries == "SITP":
+  print("That's cool!")
+  favcharacter = input("Who is your favorite character? ")
+  if favcharacter == "Conrad":
+    print("That's my favorite character too!")
+  elif favcharacter == "Jeremiah":
+    print("Well, that's fine but not my favourite.")
+  else:
+    print("Are you sure you have watched the series or you lying?")
+else:
+  print("Oh, that's not my favorite show.")
