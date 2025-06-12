6b627083c4fdf2411a81efce06669ec335fc2bbc
Khushi-Allawadi <khushiallawadi251@gmail.com>
2025-06-12 20:39:52 +0530

    ✅ Day 8 - completed

---
+++ Day08/Day08.py
@@ -0,0 +1,27 @@
+#come here for daily affirmations
+print("Welcome to the daily affirmations program!")
+name = input("What is your name? ")
+
+if name == "Aashvi" or name == "Aashvi":
+  day = input("Hello Khushi! How's your day today?")
+  if day == "good" or day == "Good":
+    week = input("That's great! What day it is today?")
+    if week == "Monday" or week == "monday":
+      print("Hope you are having a great start of the week!", name)
+    elif week == "Tuesday" or week == "tuesday":
+      print("Hope Tuesdays are always amazing for you!", name)
+    elif week == "Wednesday" or week == "wednesday":
+      print("You made it to the mid day yay, that's amazing!", name)
+    elif week == "Thursday" or week == "thursday":
+      print("You are almost there, keep going!", name)
+    elif week == "Friday" or week == "friday":
+      print("Yay, it's the weekend, you made it!", name)
+    elif week == "Saturday" or week == "saturday":
+      print("Don't forget to part hard!", name)
+    elif week == "Sunday" or week == "sunday":
+      print("Hope you had a great weekend!", name)
+    else:
+      print("That's not a day of the week, try again!")
+
+else:
+  print("Hello", name, "I hope you have a great day!")
