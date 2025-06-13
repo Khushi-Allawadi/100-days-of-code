f5ea2e8881ea4dadfd4bdbfbd53cd287b838c0ae
Khushi-Allawadi <khushiallawadi251@gmail.com>
2025-06-13 16:57:06 +0530

    ✅ Day 9 it is

---
+++ Day09/day09.py
@@ -0,0 +1,15 @@
+#generation generator
+gen = int(input("What year were you born in?"))
+if gen >=1925 and gen <= 1946:
+  print("God! huge respect man. Oldie but a Traditionalist, wow.")
+elif gen >= 1947 and gen <= 1964:
+  print("Baby Boomer, you're a legend! lessgo")
+elif gen >= 1965 and gen <= 1981:
+  print("Gen X, not too bad, we'll not call you old haha!")
+elif gen >= 1982 and gen <= 1995:
+  print("Millenial, well well, you are a oldie hehe")
+elif gen >= 1996 and gen <= 2015:
+  print("Gen Z, welcome to the community baby!")
+else:
+  print("How are you even alive?")
+  
